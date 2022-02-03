8a55312e77a77d2f7eb0c438b8b8226b1fba198b
satdevops88 <sat.devops88@gmail.com>
2022-02-03 08:11:55 -0600

    Update Hero and add Second section

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Layout from '@components/Layout/Layout';
 import Hero from '@sections/Home/Hero/Hero';
 import content from '@utils/textContent';
+import Introduction from '@sections/Home/Introduction/Introduction';
 
 
 const Home = () => {
-    const { hero } = content;
+    const { hero, introduction } = content;
     return (
         <Layout>
             <Hero content={hero} />
+            <Introduction content={introduction} />
             <div></div>
         </Layout>
     );
