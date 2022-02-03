f2e4f7095e52c36d83e3ad7ac76eb3ad0dcd7422
satdevops88 <sat.devops88@gmail.com>
2022-02-03 09:25:34 -0600

    Add Experiences section with Card components

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,16 @@ import Layout from '@components/Layout/Layout';
 import Hero from '@sections/Home/Hero/Hero';
 import content from '@utils/textContent';
 import Introduction from '@sections/Home/Introduction/Introduction';
+import Experiences from '@sections/Home/Experiences/Experiences';
 
 
 const Home = () => {
-    const { hero, introduction } = content;
+    const { hero, introduction, experiences } = content;
     return (
         <Layout>
             <Hero content={hero} />
             <Introduction content={introduction} />
+            <Experiences content={experiences} />
             <div></div>
         </Layout>
     );
