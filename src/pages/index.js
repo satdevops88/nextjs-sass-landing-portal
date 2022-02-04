1c60210ea24538ba59c1114032a29b1279260940
satdevops88 <sat.devops88@gmail.com>
2022-02-04 01:04:50 -0600

    Add Events section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import Layout from '@components/Layout/Layout';
 import Hero from '@sections/Home/Hero/Hero';
-import content from '@utils/textContent';
 import Introduction from '@sections/Home/Introduction/Introduction';
 import Experiences from '@sections/Home/Experiences/Experiences';
+import Events from '@sections/Home/Events/Events';
+import content from '@utils/textContent';
 
 
 const Home = () => {
-    const { hero, introduction, experiences } = content;
+    const { hero, introduction, experiences, events } = content;
     return (
         <Layout>
             <Hero content={hero} />
             <Introduction content={introduction} />
             <Experiences content={experiences} />
+            <Events content={events} />
             <div></div>
         </Layout>
     );
